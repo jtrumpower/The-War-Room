16c969db0b910b2be68ec68aebbb2294aa9c2fdd
jtrumpower <trumpower102@gmail.com>
2016-02-15 21:36:11 -0500

    fixed model issue

--- war_room_app/models.py
+++ war_room_app/models.py
@@ -6,42 +6,42 @@ from django.db import models
 # Create your models here.
 
 class Wars(models.Model):
-  idwars = models.IntegerField(primary_key=True, max_length=11)
-  size = models.IntegerField(max_length=11)
+  idwars = models.IntegerField(primary_key=True)
+  size = models.IntegerField()
   starttime = models.DateTimeField()
   message = models.CharField(max_length=500)
   clanid = models.CharField(max_length=45)
   enemyclanid = models.CharField(max_length=45)
   stars = models.IntegerField(max_length=11)
-  enemystars = models.IntegerField(max_length=11)
+  enemystars = models.IntegerField()
   destruction = models.DecimalField(max_digits=10, decimal_places=3)
   enemydestruction = models.DecimalField(max_digits=10, decimal_places=3)
 
 
 class Members(models.Model):
-  idmembers = models.IntegerField(primary_key=True, max_length=11)
+  idmembers = models.IntegerField(primary_key=True)
   fullname = models.CharField(max_length=300)
   gamename = models.CharField(max_length=300)
   thumbnail = models.ImageField()
-  totalstars = models.IntegerField(max_length=11)
-  
+  totalstars = models.IntegerField()
+
 
 class Bases(models.Model):
-  idbases = models.IntegerField(primary_key=True, max_length=11)
+  idbases = models.IntegerField(primary_key=True)
   name = models.CharField(max_length=200)
   idwars = models.ForeignKey(Wars, on_delete=models.CASCADE)
 
 
 class Calls(models.Model):
-  idcalls = models.IntegerField(primary_key=True, max_length=11)
+  idcalls = models.IntegerField(primary_key=True)
   idmembers = models.ForeignKey(Members, on_delete=models.CASCADE)
   idbases = models.ForeignKey(Bases, on_delete=models.CASCADE)
-  stars = models.IntegerField(max_length=11)
+  stars = models.IntegerField()
   time = models.DateTimeField()
 
 
 class Comments(models.Model):
-  idcomments = models.IntegerField(primary_key=True, max_length=11)
+  idcomments = models.IntegerField(primary_key=True)
   idbases = models.ForeignKey(Bases, on_delete=models.CASCADE)
   idmembers = models.ForeignKey(Members, on_delete=models.CASCADE)
   comment = models.CharField(max_length=3000)
