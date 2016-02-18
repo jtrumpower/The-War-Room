9d9b5439955c9de3d4a6a843bd7ea6ac0742bb24
Josiah Trumpower <trumpower102@gmail.com>
2016-02-18 00:02:03 -0500

    added angular and Django REST Framework

--- war_room_app/models.py
+++ war_room_app/models.py
@@ -1,42 +1,65 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 
 
 # Create your models here.
+@python_2_unicode_compatible  # only if you need to support Python 2
 class War(models.Model):
+  title = models.CharField(max_length=100,null=True,blank=True)
   size = models.IntegerField()
-  starttime = models.DateTimeField()
-  message = models.CharField(max_length=500)
-  clanid = models.CharField(max_length=45)
-  enemyclanid = models.CharField(max_length=45)
-  stars = models.IntegerField()
-  enemystars = models.IntegerField()
-  destruction = models.DecimalField(max_digits=10, decimal_places=3)
-  enemydestruction = models.DecimalField(max_digits=10, decimal_places=3)
+  start_time = models.DateTimeField(auto_now=True,)
+  message = models.CharField(max_length=500,null=True,blank=True)
+  clan_id = models.CharField(max_length=45)
+  enemy_clan_id = models.CharField(max_length=45)
+  stars = models.IntegerField(default=0)
+  enemy_stars = models.IntegerField(default=0)
+  destruction = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
+  enemy_destruction = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
+
+  def __str__(self):
+    return self.title
 
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Member(models.Model):
-  fullname = models.CharField(max_length=300)
-  gamename = models.CharField(max_length=300)
-  thumbnail = models.ImageField(null=True)
-  totalstars = models.IntegerField()
+  full_name = models.CharField(max_length=300,null=True,blank=True)
+  game_name = models.CharField(max_length=300)
+  thumbnail = models.ImageField(null=True,upload_to="./static/photos",blank=True)
+  total_stars = models.IntegerField(default=0)
 
+  def __str__(self):
+    return self.game_name
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Base(models.Model):
-  name = models.CharField(max_length=200)
   war = models.ForeignKey(War, on_delete=models.CASCADE)
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
 
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Dib(models.Model):
   member = models.ForeignKey(Member, on_delete=models.CASCADE)
   base = models.ForeignKey(Base, on_delete=models.CASCADE)
-  stars = models.IntegerField()
-  time = models.DateTimeField()
+  title = models.CharField(max_length=100,blank=True,null=True)
+  stars = models.IntegerField(default=0)
+  time = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.title
 
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Comment(models.Model):
   base = models.ForeignKey(Base, on_delete=models.CASCADE)
   member = models.ForeignKey(Member, on_delete=models.CASCADE)
+  title = models.CharField(max_length=100,blank=True,null=True)
   comment = models.CharField(max_length=3000)
-  type = models.CharField(max_length=300)
+  type = models.CharField(max_length=300,choices=(('comment','comment'), ('review','review')),default='comment')
+
+  def __str__(self):
+    return self.title
--- war_room_app/views.py
+++ war_room_app/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
 # Create your views here.
+from war_room_app.models import War,Member,Base,Dib,Comment
+from war_room_app.serializers import WarSerializer,MemberSerializer,BaseSerializer,DibSerializer,CommentSerializer
+from rest_framework import generics
+
+
+class WarList(generics.ListCreateAPIView):
+  queryset = War.objects.all()
+  serializer_class = WarSerializer
+
+
+class WarDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = War.objects.all()
+  serializer_class = WarSerializer
+
+
+class MemberList(generics.ListCreateAPIView):
+  queryset = Member.objects.all()
+  serializer_class = MemberSerializer
+
+
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Member.objects.all()
+  serializer_class = MemberSerializer
+
+
+class BaseList(generics.ListCreateAPIView):
+  queryset = Base.objects.all()
+  serializer_class = BaseSerializer
+
+
+class BaseDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Base.objects.all()
+  serializer_class = BaseSerializer
+
+
+class DibList(generics.ListCreateAPIView):
+  queryset = Dib.objects.all()
+  serializer_class = DibSerializer
+
+
+class DibDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Dib.objects.all()
+  serializer_class = DibSerializer
+
+
+class CommentList(generics.ListCreateAPIView):
+  queryset = Comment.objects.all()
+  serializer_class = CommentSerializer
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Comment.objects.all()
+  serializer_class = CommentSerializer
